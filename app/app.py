ea47b085d4f99ff6cb963bfea79823b033032c78
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-13 22:12:29 +0200

    specific post endpoint using id creation

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
 
 app = FastAPI()
 
-text_posts = {}
+text_posts = {
+    1:{"title": "first Post", "content": "This is my first post ever using UV instead of pip"},
+}
 
 # creating endpoint
 @app.get("/posts")
 def get_all_posts():
     return text_posts
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found")
+    
+    return text_posts.get(id)
