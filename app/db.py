9330c852634ed40007a4d6c7a1bf08d47172b71f
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-14 22:50:45 +0200

    databse setup

---
+++ app/db.py
@@ -0,0 +1,12 @@
+from collections.abc import AsyncGenerator
+import uuid
+
+# database imports 
+from sqlalchemy import Column, String, Text, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, relationship
+
+# url of database local / remote
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
