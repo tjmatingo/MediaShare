49269ecc9c717095c8ccccd517f3580f79d044d3
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-14 23:13:27 +0200

    db datetime import fix

--- app/db.py
+++ app/db.py
@@ -1,12 +1,12 @@
 from collections.abc import AsyncGenerator
 import uuid
-import datetime
 
 # database imports 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime
 
 # url of database local / remote
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
