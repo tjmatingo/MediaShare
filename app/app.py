604cb9e17db3ee926651da5216c9adc98efd4cff
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-16 23:31:49 +0200

    file upload endpoint

--- app/app.py
+++ app/app.py
@@ -3,11 +3,19 @@ from app.schemas import PostCreate, PostResponse
 
 # db imports 
 from app.db import Post, create_db_and_tables, get_async_session
+from app.images import imagekit
+import shutil
+import os 
+import tempfile
+import uuid
+
+
 
 # to create db automatically if not yet created 
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,27 +33,52 @@ async def upload_file(
     session: AsyncSession = Depends(get_async_session)
 ):
     
-    '''
-    how to create a new object in db
-    ~ Create object
-    ~session.add(object)
-    ~await sesssion.commit()
-    ~await session.refresh(object)
+    temp_file_path = None
 
-    refreshing populates the object with missing data fields like id and created_at in this context
-    '''
-    post = Post(
-        caption=caption,
-        url="dummy url",
-        file_type="photo",
-        file_name="dummy name"
-    )
+    try: 
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(f"{file.filename}")[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
 
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
+            upload_result = imagekit.files.upload(
+                file=open(temp_file_path, "rb"), 
+                file_name=f"{file.filename}",
+                use_unique_file_name=False,
+                tags=["backend-upload"],
+            )
 
-    return post
+            if upload_result == 200:
+                
+                '''
+                how to create a new object in db
+                ~ Create object
+                ~session.add(object)
+                ~await sesssion.commit()
+                ~await session.refresh(object)
+
+                refreshing populates the object with missing data fields like id and created_at in this context
+                '''
+                post = Post(
+                    caption=caption,
+                    url=upload_result.url,
+                    file_type="video" if file.content_type.startswith("video/") else "image",
+                    file_name=upload_result.name,
+                )
+
+                session.add(post)
+                await session.commit()
+                await session.refresh(post)
+
+                return post
+
+    except Exception as e: 
+        raise HTTPException(status_code=500, detail=str(e))
+    finally: 
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
+        
+    
 
 
 # endpoint to see posts
@@ -66,7 +99,7 @@ async def get_feed(
                 "caption": post.caption,
                 "url": post.url,
                 "file_type": post.file_type,
-                'file_name': post.file_name,
+                "file_name": post.file_name,
                 "created_at": post.created_at.isoformat(),
             }
         )
