7c8672f0ef7bac2a13c7a3ffc6000a2d00c959c7
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-16 23:38:45 +0200

    images connection to imagekitio fix

--- app/images.py
+++ app/images.py
@@ -3,5 +3,9 @@ from imagekitio import ImageKit
 import os
 
 load_dotenv()
+imagekit = ImageKit(
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY")
+)
 
-imagekit = ImageKit()
+# Store URL endpoint for reuse
+URL_ENDPOINT = os.getenv("IMAGEKIT_URL_ENDPOINT")
