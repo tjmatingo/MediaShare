be0360c5fee0ecced4de0eb29288cab2f68315b2
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-18 21:03:11 +0200

    users auth setup

---
+++ app/users.py
@@ -0,0 +1,7 @@
+import uuid
+from typing import Optional
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin, models
+from fastapi_users.authentication import AuthenticationBackend, BearerTransport, JWTStrategy
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from app.db import User, get_user_db
