40f971eef72167faffe1450b33d100df56228cc4
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-18 21:31:44 +0200

    users JWT token setup

--- app/users.py
+++ app/users.py
@@ -5,3 +5,9 @@ from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin, models
 from fastapi_users.authentication import AuthenticationBackend, BearerTransport, JWTStrategy
 from fastapi_users.db import SQLAlchemyUserDatabase
 from app.db import User, get_user_db
+
+SECRET = "Iub%i$56ua4bi$u6UI4HIS6U#$6%4^$5646$%^3"
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
