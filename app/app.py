cc2dbaf19b229c6372f7945f1ca2a1f753e73a5f
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-19 03:38:34 +0200

    app.py decomment fix

--- app/app.py
+++ app/app.py
@@ -123,7 +123,7 @@ async def get_feed(
                 "file_name": post.file_name,
                 "created_at": post.created_at.isoformat(),
                 "is_owner": post.user_id == user.id,
-                # "email": user_dict.get(post.user_id, "User not Found😢")
+                "email": user_dict.get(post.user_id, "User not Found😢")
             }
         )
 
